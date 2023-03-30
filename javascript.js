4ae1490899099a6dedc746086a2d097b49efed87
whatsmynameagain <43839098+whatsmynameagain@users.noreply.github.com>
2023-03-30 03:06:54 -0300

    Grid toggle added Aside from looking up and testing a few things and implementing this new small functionality, the project still has no real shape or much planning done yet.

--- javascript.js
+++ javascript.js
@@ -1,13 +1,59 @@
 
 
 let gridSize = 16;
+let showBorders = true;
+let bordersCSS;
+const containerSize = 480;
+const container = document.querySelector('#container')
+const bordersCheckbox = document.querySelector('#bordersCheckbox')
+
+bordersCheckbox.addEventListener('click', () => {
+    showBorders = !showBorders;
+    toggleBorders();
+})
+
+function toggleBorders() {
+    let children = document.querySelectorAll(".blockdefault");
+    if (children.length != 0) { 
+        
+        if (showBorders) {
+            children.forEach ( child => {
+                child.classList.remove("blocknoborders") 
+                child.classList.add("blockborders")
+            })
+        } else {
+            children.forEach ( child => {
+                child.classList.remove("blockborders")
+                child.classList.add("blocknoborders") 
+            })
+        }
+        
+        
+    }; 
+}
 
 function fillContainer(size) {
+    let children = document.querySelectorAll(".block");
+    let blockSize = Math.floor(containerSize/gridSize);
+    bordersCSS = showBorders ? "border: 1px solid grey;" : "";
+    if (children.length != 0) { 
+        children.foreach ( child => 
+            child.remove() ) 
+    }; 
+
     for (let i=0; i<size*size; i++) {
         let childDiv = document.createElement('div');
-        childDiv.classList.add("block");
-        document.querySelector('#container').appendChild(childDiv);
+        childDiv.classList.add("blockdefault");
+        childDiv.classList.add("blockborders");
+       /* childDiv.setAttribute("style", `
+            height: ${blockSize}px; 
+            width: ${blockSize}px;
+            `) 
+        */
+        container.appendChild(childDiv);
     }
+ 
+    
 }
 
 fillContainer(gridSize);
